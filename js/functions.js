f58b5256bf78d832e41904807726fcc7cb399134
OlgaFeish <108367203+OlgaFeish@users.noreply.github.com>
2023-08-19 01:12:49 +0300

    fix task1

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,31 @@
+//добавляет символы
+const addSymbols = function (string, minLength, stringAdd) {
+  let newString;
+
+  if (string.length >= minLength) {
+    newString = string;
+  } else {
+
+    if ((string.length) + (stringAdd.length) >= minLength) {
+      newString = stringAdd.slice(0, minLength - string.length) + string;
+    } else {
+
+      while (minLength - (string.length) >= (stringAdd.length)) {
+        string = stringAdd + string;
+      }
+      newString = stringAdd.slice(0, minLength - string.length) + string;
+    }
+  }
+  return newString;
+};
+
+addSymbols('1', 2, '0');
+addSymbols('1', 4, '0');
+addSymbols('q', 4, 'werty');
+addSymbols('q', 4, 'we');
+addSymbols('qwerty', 4, '0');
+
+
 //извлекает цифры
 const getNumbers = function (string) {
   let stringNew = '';
@@ -29,45 +57,17 @@ const checkLength = (string, maxLength) => string.length <= maxLength;
 checkLength('проверяемая строка', 20);
 
 
-//добавляет символы
-let addSymbols = function (string, minLength, stringAdd) {
-
-  if (string.length + 1 >= minLength) {
-    addSymbols = string;
-    return addSymbols;
-  } else {
-
-    if ((string.length + 1) + (stringAdd.length + 1) >= minLength) {
-      addSymbols = stringAdd.slice(0, minLength - string.length) + string;
-      return addSymbols;
-    } else {
-
-      while (minLength - (string.length + 1) >= (stringAdd.length + 1)) {
-        string = stringAdd + string;
-        return string;
-      }
-      string = stringAdd.slice(0, minLength - string.length) + string;
-    }
-    return string;
-  }
-};
-
-addSymbols('1', 2, '0');
-addSymbols('1', 4, '0');
-addSymbols('q', 4, 'werty');
-addSymbols('q', 4, 'we');
-addSymbols('qwerty', 4, '0');
-
-
 //проверяет палиндром
 const isPalindrome = function (string) {
 
   const stringModified = string.toLowerCase().replaceAll(' ', '');
+  let stringPalindrome;
 
-  for (let i = 0; i < ((stringModified.length + 2) / 2); i++) {
-    const stringPalindrome = (stringModified.at(i) !== stringModified.at(-(i + 1))) ? 'false' : 'true' ;
-    return stringPalindrome;
+  for (let i = 0; i < (stringModified.length / 2); i++) {
+    stringPalindrome = (stringModified.at(i) !== stringModified.at(-(i + 1))) ? 'false' : 'true' ;
   }
+  return stringPalindrome;
 };
 
 isPalindrome('Лёша на полке клопа нашёл ');
+isPalindrome('Лёша на полке нашёл ');
