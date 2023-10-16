ab2e52785894b970f26dd58da3413f527b5ce527
OlgaFeish <108367203+OlgaFeish@users.noreply.github.com>
2023-10-16 22:48:10 +0300

    9-1

---
+++ js/form.js
@@ -0,0 +1,59 @@
+const HASHTAGS_MAX_NUMBER = 5;
+const formElement = document.querySelector('#upload-select-image');
+const hashtagsInputElement = formElement.querySelector('.text__hashtags');
+
+const pristine = new Pristine(formElement, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+});
+
+const onSubmit = (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    formElement.submit();
+  }
+};
+
+const normalizeTags = (value) => value
+  .trim()
+  .split(' ')
+  .filter((tag) => Boolean(tag.length));
+
+const validateuniqueHashtags = (value) => {
+  const hashtags = normalizeTags(value);
+  const uniqueHashtags = new Set(hashtags.map((hashtag) => hashtag.toLowerCase()));
+  return hashtags.length === uniqueHashtags.size;
+};
+
+const validateRegexpHashtags = (value) => {
+  const hashtags = normalizeTags(value);
+  const hashtagRegexp = /^#[a-zа-яё0-9]{1,19}$/i;
+  return hashtags.find((hashtag) => !hashtagRegexp.test(hashtag)) === undefined;
+};
+
+const validateHashtagsNumber = (value) => {
+  const hashtags = normalizeTags(value);
+  return hashtags.length <= HASHTAGS_MAX_NUMBER;
+};
+
+pristine.addValidator(
+  hashtagsInputElement,
+  validateRegexpHashtags,
+  'введены недопустимые символы'
+);
+
+pristine.addValidator(
+  hashtagsInputElement,
+  validateuniqueHashtags,
+  'хештеги не должны повторяться'
+);
+
+pristine.addValidator(
+  hashtagsInputElement,
+  validateHashtagsNumber,
+  'максимальное количество хештегов: 5'
+);
+
+export { onSubmit };
